be93df4c2ba40d5f46fccee38af1944efc55428f
Andrew Stegmaier <andrew.stegmaier@microsoft.com>
2023-12-21 11:44:36 -0500

    Move back to regular folders.

--- index.js
+++ index.js
@@ -9,15 +9,15 @@ scenarioDropdown.onchange = updateScenario;
 let currentScenario;
 function updateScenario() {
   currentScenario = scenarioDropdown.value;
-  fetch(`./_includes/${currentScenario}/index.js`)
+  fetch(`./${currentScenario}/index.js`)
     .then((response) => response.text())
     .then((code) => (document.getElementById("code").textContent = code))
     .then(() => hljs.highlightAll());
-  fetch(`./_includes/${currentScenario}/iframe.js`)
+  fetch(`./${currentScenario}/iframe.js`)
     .then((response) => response.text())
     .then((code) => (document.getElementById("code-iframe").textContent = code))
     .then(() => hljs.highlightAll());
-  fetch(`./_includes/${currentScenario}/description.md`)
+  fetch(`./${currentScenario}/index.md`)
     .then((response) => response.blob())
     .then((blob) => blob.text())
     .then((markdown) => (document.getElementById("scenario-description").innerHTML = marked.parse(markdown)));
