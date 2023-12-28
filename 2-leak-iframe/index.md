16e5f585b5628cb2108188ec442e41b4b1df2500
Andrew Stegmaier <andrew.stegmaier@microsoft.com>
2023-12-28 12:03:42 -0600

    Store current scenario in query params.

--- 2-leak-iframe/index.md
+++ 2-leak-iframe/index.md
@@ -1,3 +1,3 @@
-# Scenario 1 - Leaking An Iframe Directly
+# Scenario 2 - Leaking An Iframe Directly
 
 This scenario illustrates what happens when the reference to the iframe is leaked directly by the main window. You can see that _only_ leaking the HTMLIframeElement is not a big deal because its window (and realm) still get garbage collected (i.e. all the bigObject arrays that get created will be cleaned up).
--- index.js
+++ index.js
@@ -5,9 +5,31 @@ import { updateScenarioDescription } from "./helpers/updateScenarioDescription.j
 
 let runCount = 0;
 
+// The scenarioDropdown is the "source of truth" for our app's state.
 const scenarioDropdown = document.getElementById("scenario");
-scenarioDropdown.onchange = (e) => updateScenarioDescription(e.currentTarget.value);
+const validScenarios = new Set(Array.from(scenarioDropdown.options).map((option) => option.value));
+
+// Set the initial scenario from the url, if possible.
+function tryGetScenarioFromQuery() {
+  const scenarioId = new URLSearchParams(window.location.search).get("scenario");
+  return validScenarios.has(scenarioId) ? scenarioId : scenarioDropdown.options[0].value;
+}
+scenarioDropdown.value = tryGetScenarioFromQuery();
 updateScenarioDescription(scenarioDropdown.value);
+
+// Changes to the dropdown should be reflected in the url and the app UI (and vice versa.)
+scenarioDropdown.onchange = (e) => {
+  const scenarioId = e.currentTarget.value;
+  const url = new URL(window.location.href);
+  url.searchParams.set("scenario", scenarioId);
+  history.pushState({}, "", url);
+  updateScenarioDescription(scenarioId);
+};
+
+window.addEventListener("popstate", (e) => {
+  scenarioDropdown.value = tryGetScenarioFromQuery();
+  updateScenarioDescription(scenarioDropdown.value);
+});
 
 initializeFinalizationRegistry();
 
