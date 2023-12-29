1ee95cf89db4abfe5dfb2125ddc90eb38c9b192e
Andrew Stegmaier <andrew.stegmaier@microsoft.com>
2023-12-29 11:30:23 -0600

    Get markdown and hlightjs from cloudpack.

--- helpers/updateScenarioDescription.js
+++ helpers/updateScenarioDescription.js
@@ -1,3 +1,7 @@
+import markdownit from "https://cdn.skypack.dev/markdown-it";
+import hljs from "https://cdn.skypack.dev/highlight.js";
+const md = markdownit();
+
 /**
  * Updates the UI in the app that describes the scenario and displays the code that will run inside the iframe and the main page.
  * @param {string} scenarioId
@@ -5,14 +9,22 @@
 export function updateScenarioDescription(scenarioId) {
   fetch(`./${scenarioId}/index.js`)
     .then((response) => response.text())
-    .then((code) => (document.getElementById("code").textContent = code))
-    .then(() => hljs.highlightAll());
+    .then((code) => {
+      const codeContainer = document.getElementById("code");
+      codeContainer.textContent = code;
+      delete codeContainer.dataset.highlighted;
+      hljs.highlightElement(codeContainer);
+    });
   fetch(`./${scenarioId}/iframe.js`)
     .then((response) => response.text())
-    .then((code) => (document.getElementById("code-iframe").textContent = code))
-    .then(() => hljs.highlightAll());
+    .then((code) => {
+      const iframeCodeContainer = document.getElementById("code-iframe");
+      iframeCodeContainer.textContent = code;
+      delete iframeCodeContainer.dataset.highlighted;
+      hljs.highlightElement(iframeCodeContainer);
+    });
   fetch(`./${scenarioId}/index.md`)
     .then((response) => response.blob())
     .then((blob) => blob.text())
-    .then((markdown) => (document.getElementById("scenario-description").innerHTML = marked.parse(markdown)));
+    .then((markdown) => (document.getElementById("scenario-description").innerHTML = md.render(markdown)));
 }
