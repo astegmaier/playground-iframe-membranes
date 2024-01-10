f18d9575fad44b6bcaddf789fd77234a69a1f614
Andrew Stegmaier <andrew.stegmaier@microsoft.com>
2024-01-10 16:50:03 -0500

    Better handling for async transpilation.

--- helpers/getTranspiledModules.js
+++ helpers/getTranspiledModules.js
@@ -1,22 +1,36 @@
 const transpilerWorker = new Worker("/helpers/transpiler-worker.js");
-transpilerWorker.addEventListener("message", (e) => {
-  // TODO: make the promise there from the beginning.
-  // TODO: refactor with async functions?
-  transpiledScripts[e.data.scriptUrl] = new Promise(async (resolve) => {
-    const module = await import("data:text/javascript;base64," + btoa(e.data.transpiledCode));
-    resolve(module);
-  });
+transpilerWorker.addEventListener("message", async (e) => {
+  const { scriptUrl, transpiledCode, error } = e.data;
+  if (error) {
+    resolveAndRejectFns[scriptUrl]?.rejectFn(error);
+  } else {
+    const module = await import("data:text/javascript;base64," + btoa(transpiledCode));
+    resolveAndRejectFns[scriptUrl]?.resolveFn(module);
+  }
 });
 
 /** @type {{[scriptUrl: string]: Promise<string> }} */
-const transpiledScripts = {};
+const transpiledModulePromises = {};
+
+/** @type {{[scriptUrl: string]: {resolveFn: (result: any) => void, rejectFn: (reason: any) => void}}} */
+const resolveAndRejectFns = {};
 
 /**
- * Returns an object containing promises for transpiled code for each scriptUrl.
+ * Returns an object containing promises for transpiled code for each scriptUrl in the array.
  * @param {string[]} scriptPaths Paths to the typescript files of the modules you want to import.
  * @returns {{[scriptUrl: string]: Promise<any> }} An object promises that resolve to each imported transpiled modules.
  */
 export function getTranspiledModules(scriptPaths) {
-  scriptPaths.forEach((url) => transpilerWorker.postMessage(url));
-  return transpiledScripts;
+  scriptPaths.forEach((url) => {
+    if (!transpiledModulePromises[url]) {
+      let resolveFn, rejectFn;
+      transpiledModulePromises[url] = new Promise((resolve, reject) => {
+        resolveFn = resolve;
+        rejectFn = reject;
+      });
+      resolveAndRejectFns[url] = { resolveFn, rejectFn };
+      transpilerWorker.postMessage(url);
+    }
+  });
+  return transpiledModulePromises;
 }
--- helpers/transpiler-worker.js
+++ helpers/transpiler-worker.js
@@ -2,8 +2,13 @@ importScripts("https://cdnjs.cloudflare.com/ajax/libs/typescript/5.3.3/typescrip
 
 onmessage = async (e) => {
   const scriptUrl = e.data;
-  const response = await fetch(scriptUrl);
-  const tsCode = await response.text();
-  const output = ts.transpileModule(tsCode, { compilerOptions: { module: ts.ModuleKind.ES2022 } });
-  postMessage({ scriptUrl, transpiledCode: output.outputText });
+  try {
+    const response = await fetch(scriptUrl);
+    const tsCode = await response.text();
+    const output = ts.transpileModule(tsCode, { compilerOptions: { module: ts.ModuleKind.ES2022 } });
+    postMessage({ scriptUrl, transpiledCode: output.outputText });
+  } catch (e) {
+    console.error("transpiler-worker got this error: ", e);
+    postMessage({ scriptUrl, error: e.toString() });
+  }
 };
