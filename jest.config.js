fdc262fda2d8347309ecc96cf73a8ab46159b7a9
msJorgef <90656841+msJorgef@users.noreply.github.com>
2024-01-17 09:54:10 -0800

    move UTs to separate files, decide on membrane from environment, add reporter

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,8 @@
 /** @type {import('ts-jest').JestConfigWithTsJest} */
 module.exports = {
-  preset: 'ts-jest',
-  testEnvironment: 'node',
+  preset: "ts-jest",
+  testEnvironment: "node",
+  setupFilesAfterEnv: ["./jest.setup.ts"],
+  reporters: ["default", "jest-html-reporters"],
+  verbose: true,
 };
